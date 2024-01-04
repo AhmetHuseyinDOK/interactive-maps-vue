59954c31bc6c3daed7d72216384832d63959c367
Ahmet Hüseyin Dok <off.ahmethuseyindok@gmail.com>
2024-01-04 09:21:40 +0700

    styling

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,7 +1,6 @@
 <template>
-    {{ mapData.meta }}
     <svg v-if="mapData" baseprofile="tiny" fill="#7c7c7c" :height="mapData.meta.height" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" version="1.2" :viewbox="`0 0 ${mapData.meta.width} ${mapData.meta.height}`" :width="mapData.meta.width" xmlns="http://www.w3.org/2000/svg">
-        <path @click="emit('update:selected', path.name)" v-for="path in mapData.data" :d="path.path" fill="gray" class="hover:fill-green-600" :class="selected == path.name ? 'fill-green-900':''"></path>
+        <path @click="emit('update:selected', path.name)" v-for="path in mapData.data" :d="path.path" fill="gray" class="path" :class="selected && selected == path.name ? 'selected-path':''"></path>
     </svg>
     
     <div v-else> loading</div>
@@ -23,3 +22,14 @@ onMounted(() => {
     });
 })
 </script>
+
+<style scoped>
+.path:hover {
+     fill: burlywood;
+}
+
+.selected-path {
+    fill: indianred;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
 import MapViewVue from '@/components/MapView.vue';
-
+import { ref } from 'vue';
+const selected = ref();
 </script>
 
 <template>
   <main>
-    <MapViewVue url="maps/id.json"></MapViewVue>
+    <MapViewVue class="map"  url="maps/tr.json" v-model:selected="selected"></MapViewVue>
+    <h1>{{ selected }}</h1>
   </main>
 </template>
+
+<style scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: indianred;
+  }
+</style>
