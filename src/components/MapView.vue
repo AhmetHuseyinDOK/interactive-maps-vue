cdfabd202a3fabfa8182edf1dedac6ca9a691042
Ahmet Hüseyin Dok <off.ahmethuseyindok@gmail.com>
2024-01-04 08:31:30 +0700

    remove unnecessary files

---
+++ src/components/MapView.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg baseprofile="tiny" fill="#7c7c7c" height="371" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" version="1.2" viewbox="0 0 1000 371" width="1000" xmlns="http://www.w3.org/2000/svg">
+        <path @click="emit('update:selected', path.name)" v-for="path in paths" :d="path.path" class="hover:fill-green-600" :class="selected == path.name ? 'fill-green-900':''"></path>
+    </svg>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+const paths =  ref()
+
+const emit = defineEmits(["update:selected"])
+interface Props {
+    url: string;
+    selected?: string;
+}
+
+const props = defineProps<Props>();
+onMounted(() => {
+    fetch(props.url).then((data) => {
+        paths.value = data.json();
+    });
+})
+</script>
