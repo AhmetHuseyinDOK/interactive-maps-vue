135845c1c1c6570bbc9bfd4b7091d263970e5b61
Ahmet Hüseyin Dok <off.ahmethuseyindok@gmail.com>
2024-01-14 16:23:01 +0700

    map view

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,13 +1,17 @@
 <template>
-    <svg v-if="mapData" baseprofile="tiny" fill="#7c7c7c" :height="mapData.meta.height" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" version="1.2" :viewbox="`0 0 ${mapData.meta.width} ${mapData.meta.height}`" :width="mapData.meta.width" xmlns="http://www.w3.org/2000/svg">
-        <path @click="emit('update:selected', path.name)" v-for="path in mapData.data" :d="path.path" fill="gray" class="path" :class="selected && selected == path.name ? 'selected-path':''"></path>
+    <svg v-if="mapData" fill="#7c7c7c" width="100%" height="100%" stroke="#ffffff" stroke-linecap="round"
+        stroke-linejoin="round" stroke-width="2" version="1.2" :viewBox="`0 0 ${mapData.meta.width} ${mapData.meta.height}`"
+        preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
+        <path @click="emit('update:selected', path.name)" v-for="path in mapData.data" :d="path.path" fill="gray"
+            class="path" :class="selected && selected == path.name ? 'selected-path' : ''">
+        </path>
     </svg>
-    
-    <div v-else> loading</div>
+
+    <div v-else>loading</div>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-const mapData =  ref()
+const mapData = ref<any>()
 
 const emit = defineEmits(["update:selected"])
 interface Props {
@@ -18,18 +22,19 @@ interface Props {
 const props = defineProps<Props>();
 onMounted(() => {
     fetch(props.url).then((data) => {
-        data.json().then( v => mapData.value = v );
+        data.json().then(v => mapData.value = v);
     });
 })
+
+
 </script>
 
 <style scoped>
 .path:hover {
-     fill: burlywood;
+    fill: burlywood;
 }
 
 .selected-path {
     fill: indianred;
 }
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
-import MapViewVue from '@/components/MapView.vue';
+
 import { ref } from 'vue';
-const selected = ref();
+import { countries } from '@/data/maps';
+import { codeCountryMap } from '@/data/country';
+const mapList = ref(countries)
+
 </script>
 
 <template>
   <main>
-    <MapViewVue class="map"  url="maps/tr.json" v-model:selected="selected"></MapViewVue>
-    <h1>{{ selected }}</h1>
+    <h1>Maps</h1>
+    <div class="maplist">
+      <div class="" v-for="code in mapList">
+        <a :href="`/map/${code}`">{{ codeCountryMap[code]?.name }}</a>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  h1 {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: indianred;
-  }
+h1 {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: indianred;
+}
 </style>
